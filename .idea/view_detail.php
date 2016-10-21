2666f726a247fe68414140de7f897a6720edcb64
MisterTarock <victor_pb@hotmail.fr>
2016-10-21 11:04:54 +0200

    scnd page

---
+++ .idea/view_detail.php
@@ -0,0 +1,65 @@
+<!--
+_____         .__         ____   ____.__
+/     \ _____  |__| ____   \   \ /   /|__| ______  _  __
+/  \ /  \\__  \ |  |/    \   \   Y   / |  |/ __ \ \/ \/ /
+/    Y    \/ __ \|  |   |  \   \     /  |  \  ___/\     /
+\____|__  (____  /__|___|  /    \___/   |__|\___  >\/\_/
+\/     \/        \/                     \/
+
+
+<!--This will be the view file-->
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="CSS\bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="CSS\style.css">
+    <title>Bogota Air</title> <!-- to name the page//-->
+
+</head>
+
+<body>
+
+<h1>Bogota Airlines</h1> <!--to make a big headline//-->
+<h2><b>Détail des Réservations</b></h2>
+
+Afin de pouvoir vous fournir un vol le plus agréable possible, nous désirons connaître
+quelques informations sur vous afin que fassions plus ample connaissance.
+<br>
+<br>
+
+<div id="reserv">
+    <form>
+        <div class="form-group">
+            <label for="exampleInputEmail1">Nom</label>
+            <input type="name" class="form-control" id="exampleInputname1" placeholder="Nom+Prénom">
+        </div>
+        <div class="form-group">
+            <label for="exampleInputAge1">Age</label>
+            <input type="Age" class="form-control" id="exampleInputAge1" placeholder="Age">
+        </div>
+
+        <input class="btn btn-primary" type="submit" value="Retour à la page précedente">
+        <input class="btn btn-primary" type="submit" value="Etape suivante">
+        <input class="btn btn-danger" type="submit" value="Annuler la réservation">
+
+    </form>
+
+</div>
+
+
+
+
+
+
+
+
+<?php
+
+
+?>
+
+
+</body>
+</html>
--- .idea/view_reserv.php
+++ .idea/view_reserv.php
@@ -12,12 +12,16 @@ _____         .__         ____   ____.__
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" type="text/css" href="CSS\bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="CSS\style.css">
-<title>Bogota Airlines</title> <!-- to name the page//-->
-<h1>Bogota Airlines</h1> <!--to make a big headline//-->
+    <title>Bogota</title> <!-- to name the page//-->
 </head>
 
 <body>
+
+<h1>Bogota Airlines</h1> <!--to make a big headline//-->
+<br>
+<h2><b>Reservation</b></h2>
 
 Bienvenue sur <b>AIR Columbia</b>!! Votre billet vous coutera une fortune et vous serez mal assis!
 <br>Mais nous ferons tout pour que vous passiez un agréable trajet!
@@ -51,26 +55,27 @@ Bienvenue sur <b>AIR Columbia</b>!! Votre billet vous coutera une fortune et vou
 <br>
 
 <form action="demo_form.asp">
+
+
     <table>
         <caption><b>Réservation</b></caption>
         <tr>
             <td>Destination:</td>
-            <td><input type="text" name="destination"></td>
+            <td><input class="form-control" type="text" name="destination" placeholder="Destination"></td>
         </tr>
         <tr>
             <td>Nombre de places:</td>
-            <td><input type="text" name="places"></td>
+            <td><input class="form-control" type="text" name="places" placeholder="Nombre de places"></td>
         </tr>
         <tr>
             <td>Assurance annulation</td>
             <td><input type="checkbox" ></td>
         </tr>
-
-
     </table>
-    <input type="submit" value="Etape suivante">
-    <input type="submit" value="Annuler la réservation">
-    <button onClick="">Solution</button>
+
+    <input class="btn btn-primary" type="submit" value="Etape suivante">
+    <input class="btn btn-primary" type="submit" value="Annuler la réservation">
+    <button class="btn btn-primary" onClick="">Solution</button>
 </form>
 
 
