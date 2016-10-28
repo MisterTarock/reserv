2b4e1258cf25ba095a446977d93910ebec8ca1c0
Paolo <paolodekeyzer@gmail.com>
2016-10-28 10:38:48 +0200

    the empty commit

--- controller.php
+++ controller.php
@@ -7,16 +7,3 @@
 
 //This will be the logic file were the data will be calculated
 
-if (isset($_POST['sumbit'])) {
-
-
-
-    $_session['destination']=$_POST['destination'];
-    $_session['places']=$_POST['places'];
-    $_session['insurance']=$_POST['insurance'];
-    include 'view_detail.php';
-}
-else{
-    include"view_reserv.php";
-}
-?>
--- index.php
+++ index.php
@@ -5,5 +5,17 @@
  * Date: 28-10-16
  * Time: 09:37
  */
-include 'controller.php'
+session_start();
+if (isset($_POST['sumbit'])) {
+
+
+
+    $_session['destination']=$_POST['destination'];
+    $_session['places']=$_POST['places'];
+    $_session['insurance']=$_POST['insurance'];
+    include 'view_valid.php';
+}
+else{
+    include"view_reserv.php";
+}
 ?>
