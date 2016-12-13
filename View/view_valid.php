5640ea90513fece505386756002870a11879afec
MisterTarock <victor_pb@hotmail.fr>
2016-12-13 14:33:16 +0100

    added bootstrap reseau

--- View/view_valid.php
+++ View/view_valid.php
@@ -7,7 +7,15 @@ Autor: Paolo De Keyzer, 13201
 <!DOCTYPE html>
 <html>
 <head>
-    <link rel="stylesheet" type="text/css" href="CSS\bootstrap.min.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
     <link rel="stylesheet" type="text/css" href="CSS\style.css">
     <title>Validation</title> <!-- to name the page//-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -116,7 +124,10 @@ Autor: Paolo De Keyzer, 13201
 
         <div class="row">
             <div class="col-md-12">
-                <input class="btn btn-danger" name="cancel" type="submit" value="Annuler la réservation">
+                <button class="btn btn-danger" name="cancel" type="submit">
+                    Annuler la réservation
+                    <span class="glyphicon glyphicon-warning-sign"></span>
+                </button>
             </div>
         </div>
 
